d358dafe921e08d937a44fda7e5207682711982b
Your Name <you@example.com>
2023-11-03 21:48:23 +0100

    knokke page push isaac

--- js/galleries/knokke.js
+++ js/galleries/knokke.js
@@ -1,27 +1,80 @@
 var knokkeImages = [
   {
     src: "images/knokke/knokke1.jpg"
+    
   },
   {
-    src: "images/knokke/knokke2_low.jpg",
-    highres: "images/knokke/knokke2_high.jpg",
-  },
-  {
-    src: "images/knokke/11.jpg",
-    title: "jasper",
-    place: "jasper",
-    description: "title",
+    src: "images/knokke/knokke2_high.jpg",
+    title: "Vuurwerkfestival 23",
+    description: "A-TEAM Services",
     vertical: false,
   },
   {
-    src: "images/knokke/12.jpg",
+    src: "images/knokke/knokke3_high.jpg",
+    title: "Vuurwerkfestival 19",
+    place: "HC Pyrotechnics",
+    vertical: false,
+
   },
   {
-    src: "Q8LGHWTlgbs",
-    video: true,
-  }
-]
+    src: "images/knokke/knokke4_high.jpg",
+    vertical: false,
+  },
+  {
+    src: "images/knokke/knokke5_high.jpg",
+    place: "Cultuurfestival 2022",
+    description: "sfeer",
+    vertical: false,
+  },
 
+  {
+    src: "images/knokke/knokke6.png",
+    vertical: false,
+  },
+
+  {
+    src: "images/knokke/knokke7_high.jpg",
+    place: "Cultuurfestival 2023",
+    description: "sfeer",
+    vertical: false,
+  },
+
+  //hier video vakantieplus
+
+  {
+    
+    src: "fbcBBSCtaFQ",
+    video: true,
+  },
+
+  {
+  src: "images/knokke/knokke9_high.jpg",
+  place: "Zoute Grand Prix 2019",
+  description: "sfeer",
+  vertical: false,
+},
+{
+  src: "images/knokke/knokke10_high.jpg",
+  place: "Zoute Grand Prix 2019",
+  description: "sfeer",
+  vertical: false,
+},
+
+{
+  src: "images/knokke/knokke11.jpg",
+  place: "Lichtfestival",
+  description: "2019",
+  vertical: false,
+},
+
+{
+  src: "images/knokke/knokke12.jpg",
+  place: "Lichtfestival",
+  description: "2019",
+  vertical: false,
+},
+
+]
 // src == plaats van de foto!
 // title = tekst onder de foto optioneel
 // place = tekst onder de titel optioneel
