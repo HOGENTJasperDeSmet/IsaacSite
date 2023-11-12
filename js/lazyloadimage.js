5f487209fa496251bf3c5f7d728db255db464c22
Jasper De Smet <36963387+HOGENTJasperDeSmet@users.noreply.github.com>
2023-11-12 19:39:44 +0100

    lowresit

--- js/lazyloadimage.js
+++ js/lazyloadimage.js
@@ -4,14 +4,23 @@
   const objects = document.getElementsByClassName('asyncImage');
   Array.from(objects).map((item) => {
     // Start loading image
+    console.log(window.innerWidth);
+
     const img = new Image();
-    img.src = item.dataset.src;
+    let x;
+    if (window.innerWidth < 769 && item.dataset.srcmobile) {
+      x = item.dataset.srcmobile;
+
+    } else {
+      x = item.dataset.src;
+    }
+    img.src = x;
     // Once image is loaded replace the src of the HTML element
     img.onload = () => {
       item.classList.remove('asyncImage');
-      return item.nodeName === 'IMG' ? 
-        item.src = item.dataset.src :        
-        item.style.backgroundImage = `url(${item.dataset.src})`;
+      return item.nodeName === 'IMG' ?
+        item.src = x :
+        item.style.backgroundImage = `url(${x})`;
     };
   });
 })();
